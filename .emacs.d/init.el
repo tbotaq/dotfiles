17c2bffbaa9ed576688489ee29b93224bbdc8ac8
tbotaq <140096+tbotaq@users.noreply.github.com>
2020-12-17 13:55:38 +0900

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,14 +1,3 @@
-;; hide startup screen
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("ba9be9caf9aa91eb34cf11ad9e8c61e54db68d2d474f99a52ba7e87097fa27f5" default)))
- '(inhibit-startup-screen t))
-
 ;; set Japanese as language
 (set-language-environment 'Japanese)
 
@@ -43,17 +32,14 @@
 (add-to-list 'load-path "~/path/to/your/elisp/rinari")
 (require 'rinari)
 
-;; インデントはタブではなくスペースを使用
 (setq-default indent-tabs-mode nil)
 
-;; 行末の空白をハイライト
 (setq-default show-trailing-whitespace t)
 
 (require 'auto-complete)
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
 
-;; メニューバーを非表示
 (menu-bar-mode -1)
 
 ;; yes or no => y or n
@@ -61,7 +47,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.erb?$"     . web-mode))
 
-;;; *.~ とかのバックアップファイルを作らない
+;;; *.~ 
 (setq make-backup-files nil)
-;;; .#* とかのバックアップファイルを作らない
+;;; .#* 
 (setq auto-save-default nil)
